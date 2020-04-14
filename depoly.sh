4dc923eeacf6e79f188c36c0cb4e1520ea829f6f
haifeng <xianghaifeng@deepxi.com>
2020-04-14 17:14:47 +0800

    feat: deploy.sh

--- depoly.sh
+++ depoly.sh
@@ -3,25 +3,17 @@
  # @Author: xianghaifeng
  # @Date: 2020-04-14 10:40:49
  # @LastEditors: xianghaifeng
- # @LastEditTime: 2020-04-14 17:04:31
+ # @LastEditTime: 2020-04-14 17:14:11
  ###
-echo ************************************
 echo Deploy Project
-echo ************************************
 echo git pull
 git pull
-echo ************************************
 echo yarn && yarn build
 yarn && yarn build:nologin
-echo ************************************
 echo stop && rm container
-echo ************************************
 docker stop vue-docker
 docker rm vue-docker
-echo ************************************
 echo docker-build
-echo ************************************
 docker build -t vue-docker .
-echo ************************************
-docker run -p 8888:80 -d vue-docker
+docker run -p 8081:80 -d vue-docker
 echo Deploy Project Finish
