dd61ca106a3742dea4ba4e180bb1a2b6eba31db3
hfxiang93 <48157293+hfxiang93@users.noreply.github.com>
2020-04-17 11:19:54 +0800

    Update depoly.sh
    update .

--- depoly.sh
+++ depoly.sh
@@ -6,6 +6,6 @@
  # @LastEditTime: 2020-04-14 17:14:11
  ###
 echo 'docker build'
-docker image build -t nuxt-temp
+docker image build -t nuxt-temp .
 echo 'docker run'
 docker container run -p 8002:3000 -it nuxt-temp /bin/bash
